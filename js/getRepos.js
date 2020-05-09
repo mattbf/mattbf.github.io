953a8f90792cd1c3012b6565c43a0d97a43c9624
Matthew Behan-Fossey <matthewbf8@gmail.com>
2020-05-09 13:02:42 -0600

    Added repo language and updated at information

--- js/getRepos.js
+++ js/getRepos.js
@@ -1,5 +1,24 @@
 var request = new XMLHttpRequest()
 
+var JSImage;
+var PyImage;
+var HTMLImage;
+
+function LoadImages() {
+    JSImage = new Image(20,20);
+    JSImage.src = "images/javascript.png";
+    JSImage.setAttribute('class', 'languageimage')
+
+    PyImage = new Image(20,20);
+    PyImage.src = "images/python.png";
+    PyImage.setAttribute('class', 'languageimage')
+
+    HTMLImage = new Image(20,20);
+    HTMLImage.src = "images/html.png";
+    HTMLImage.setAttribute('class', 'languageimage')
+}
+LoadImages()
+
 // Open a new connection, using the GET request on the URL endpoint
 request.open('GET', 'https://api.github.com/users/mattbf/repos', true)
 
@@ -35,9 +54,63 @@ request.onload = function () {
             } else {
                 p.textContent = `${repodescription}`
             }
+
+            //create repo info div
+            const repoinfodiv = document.createElement('div')
+            repoCard.setAttribute('class', 'repoinfo')
+            const wrapper1 = document.createElement('div')
+            repoCard.setAttribute('class', 'languagewrapper')
+            const wrapper2 = document.createElement('div')
+            repoCard.setAttribute('class', 'languagewrapper')
+
+            //Get repo language
+            if(repo.language){
+              if(repo.language === "JavaScript"){
+                wrapper1.appendChild(JSImage)
+                languagetext.textContent = "JavaScript"
+                wrapper1.appendChild(languagetext)
+              } else if(repo.language === "Python") {
+                wrapper1.appendChild(PyImage)
+                languagetext.textContent = "Python"
+                wrapper1.appendChild(languagetext)
+              } else if(repo.language === "HTML") {
+                wrapper1.appendChild(HTMLImage)
+                languagetext.textContent = "HTML"
+                wrapper1.appendChild(languagetext)
+              } else (
+                //do not append a language or image
+              )
+            }
+
+            //get repo last updated date
+            if(repo.updated_at){
+              const updatedat = document.createElement('p')
+              updatedat.setAttribute('class', 'repoinfotext.marginright')
+              date.textContent = "Update at: "
+              const date = document.createElement('p')
+              date.setAttribute('class', 'repoinfotext')
+              date.textContent = repo.updated_at.toDateString()
+              wrapper2.appendChild(updatedat)
+              wrapper2.appendChild(date)
+            }
+
+
+            //construct repo information div
+            if(repo.language){
+              repoinfodiv.appendChild(wrapper1)
+            }
+            if(repo.updated_at){
+              repoinfodiv.appendChild(wrapper2)
+            }
+
+
             // Append content to the repo card element
             repoCard.appendChild(title)
             repoCard.appendChild(p)
+            const underline = document.createElement('div')
+            underline.setAttribute('class', 'underline')
+            repoCard.appendChild(underline)
+            repoCard.appendChild(repoinfodiv)
 
             // Append the card to the div with "Cards-Container" id
             document.getElementById("Repos-Wrapper").appendChild(repoCard);
